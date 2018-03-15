cc45e740b8e64e5388a23770ebf7f40e3feb9e4d
jdron <jdron@users.noreply.github.com>
2018-03-15 10:22:17 -0400

    DEBUG - updates
    fixed up some formatting/commenting of the Exautomate script.

--- src/Exautomate.sh
+++ src/Exautomate.sh
@@ -7,34 +7,38 @@ echo "Welcome to Ex-Automate."
 choice=0
 
 while [ $choice -ne 5 ]; do
-  printf " 1: Pre-merged vcf \n 2: Merge case and control vcf for analysis. \n 3: Retrieve 1000 Genomes options \n 4: Synthetic run \n 5: Exit \n"
+  printf " 1: Pre-merged .vcf for analysis. \n 2: Merge case and control .vcf for analysis. \n 3: Retrieve 1000 Genomes, no analysis. \n 4: Synthetic run. \n 5: Exit. \n"
   read -p "Enter (1-4): " choice
+
+########## OPTION 1 ##########
+  #The user already has a merged .vcf they want to work with.
   if [ $choice -eq 1 ]; then
 
     ls ../input/*.vcf
-    read -e -p "Enter the vcf file you would like to analyze: " vcfInput
+    read -e -p "Enter the .vcf file you would like to analyze (include extension): " vcfInput
     echo ""
 
-    #If there are comments (eg lines starting with #) mid-vcf file then this command is invalid. However, there should not be.
+    #If there are comments (eg. lines starting with #) mid-vcf file, then this command is invalid. However, there should not be.
     headerLines=$(grep -o '#' $vcfInput | wc -l)
 
-    read -e -p "Enter the number of controls in your vcf file. Script assumes vcf is all controls, then all cases: " numControls
+    read -e -p "Enter the number of controls in your .vcf file (script assumes .vcf has all the controls lumped together first, then all cases): " numControls
     echo ""
 
-    read -e -p "Choose filename for processed vcf (include .vcf): " vcfOutput
+    read -e -p "Choose filename for the processed .vcf file (include extension): " vcfOutput
     echo ""
 
-    read -e -p "Choose filename for output plink files (no extension): " plinkOutput
+    read -e -p "Choose filename for the output PLINK files (no extension): " plinkOutput
     echo ""
 
-  echo "Kernels: linear, linear.weighted, quadratic, IBS, 2wayIX"
+  ### TO DO: make a file called kernellist.txt with all valid kernel names. ###
+  echo "Kernel options: linear, linear.weighted, quadratic, IBS, 2wayIX"
   read -p "Enter the kernel to be used in the analysis: " kernel
   echo ""
 
-#Handles the choice of methods that are available for different kernels.
+  #Handles the choice of methods that are available for different kernels.
   if [ "$kernel" == "linear" ] || [ "$kernel" == "linear.weighted" ]; then
-    read -p "Choose SKAT or SKAT-O: " choice
-    if [ "$choice" == "SKAT-O" ]; then
+    read -p "Choose SKAT ( 1 ) or SKAT-O ( 2 ): " choice
+    if [ "$choice" == 2 ]; then
       method = "optimal.adj"
     else
       method = "davies"
@@ -44,40 +48,36 @@ while [ $choice -ne 5 ]; do
   fi
 
   ./ExautomateBackEnd.sh ../dependencies/hg_19.fasta $vcfInput $vcfOutput $headerLines $plinkOutput $kernel $numControls $method
-    #make a file called kernellist.txt with all valid kernel names.
 
+########## OPTION 2 ##########
+  #The user has two merged .vcf files (one case, one control) they want to work with.
   elif [ $choice -eq 2 ]; then
 
     ls  ../input/*.vcf
-    read -e -p 'Enter the name of the control vcf: ' controlvcf
+    read -e -p "Enter the name of the control .vcf file (include extension): " controlvcf
     numControls=$(awk '{if ($1 == "#CHROM"){print NF-9; exit}}' $controlvcf)
     echo "Detecting " $numControls " controls"
     echo ""
 
     ls  ../input/*.vcf
-    read -e -p "Enter the name of the cases vcf: " casesvcf
+    read -e -p "Enter the name of the case .vcf file (include extension): " casesvcf
     echo ""
 
-    #make sure it is .vcf
-    read -e -p "Enter the name of the output file: " vcfInput
+    read -e -p "Enter the name of the final merged .vcf file (include extension): " vcfInput
 
     ./MergeVCFs.sh $controlvcf $casesvcf ../dependencies/hg19.fasta $vcfInput
 
-    #read -e -p "Enter the desired name of the processed vcf: "
-
-    read -e -p "Choose filename for output plink files (no extension): " plinkOutput
+    read -e -p "Choose filename for the output PLINK files (no extension): " plinkOutput
     echo ""
 
-
-    #Add list of kernels for user to see.
+    ### TO DO: make a file called kernellist.txt with all valid kernel names. ###
+    echo "Kernel options: linear, linear.weighted, quadratic, IBS, 2wayIX"
     read -e -p "Enter the kernel to be used in the analysis: " kernel
     echo ""
 
-  #Handles the choice of methods that are available for different kernels.
+    #Handles the choice of methods that are available for different kernels.
     if [ "$kernel" == "linear" ] || [ "$kernel" == "linear.weighted" ]; then
-
       read -p "Choose SKAT ( 1 ) or SKAT-O ( 2 ): " choice
-
       if [ "$choice" == 2 ]; then
         method = "optimal.adj"
       else
@@ -85,19 +85,17 @@ while [ $choice -ne 5 ]; do
       fi
 
     #Default to davies if the kernel can't do SKAT-O.
-
     else
       method = "davies"
     fi
 
-
   ./ExautomateBackEnd.sh ../dependencies/hg_19.fasta $vcfInput $vcfOutput $headerLines $plinkOutput $kernel $numControls $method
 
-
-
+########## OPTION 3 ##########
+  #The user needs the 1000 Genomes data. This option does not perform SKAT.
+  #Requires wget and vcftools.
+  #Fragile. If the location of the 1000 Genome files are moved, then this will fail.
   elif [ $choice -eq 3 ]; then
-  #Requires wget and vcftools
-  #Fragile. If the location of the 1000 genome files are moved then this will fail.
 
     mkdir ./1000gvcf
 
@@ -142,43 +140,43 @@ while [ $choice -ne 5 ]; do
         rm ./1000gvcf/*
     fi
 
-
+########## OPTION 4 ##########
+  #The user wants to generate a synthetic dataset for SKAT analysis.
   elif [ $choice -eq 4 ]; then
 
     ls ../input/*.sim
-    read -p "Enter the filename of the .sim file to be used: " simInput
+    read -p "Enter the filename of the .sim file to be used (include extension): " simInput
     echo ""
 
+    ### TO DO: make a file called kernellist.txt with all valid kernel names. ###
+    echo "Kernel options: linear, linear.weighted, quadratic, IBS, 2wayIX"
+    read -p "Enter the kernel to be used in the analysis: " kernel
+    echo ""
 
-  read -p "Enter the kernel to run on the synthetic files: " kernel
-  echo ""
-  #Handles the choice of methods that are available for different kernels.
-    if [ "$kernel" == "linear" ] || [ "$kernel" == "linear.weighted" ]; then
-      read -p "Choose SKAT or SKAT-O: " choice
-      if [ "$choice" == "SKAT-O" ]; then
-        method = "optimal.adj"
+    #Handles the choice of methods that are available for different kernels.
+      if [ "$kernel" == "linear" ] || [ "$kernel" == "linear.weighted" ]; then
+        read -p "Choose SKAT ( 1 ) or SKAT-O ( 2 ): " choice
+        if [ "$choice" == 2 ]; then
+          method = "optimal.adj"
+        else
+          method = "davies"
+        fi
       else
         method = "davies"
       fi
+
+      read -p "Choose filename for the output PLINK files (no extension): " outputName
+      echo ""
+
+      ./synthesizeSKATFiles.sh $simInput $outputName
+
+      echo "Running SKAT"
+      Rscript RunSkat.R $outputName.bed $outputName.bim $outputName.fam $outputName.bim.SetID "SSD_File.SSD" $kernel $method
+      echo "SKAT complete."
+      mv $outputName.* ../output/$outputName.*
+
     else
-      method = "davies"
+      echo "Unknown input."
     fi
 
-    read -p "Enter the filename for the output: " outputName
-    echo ""
-
-    ./synthesizeSKATFiles.sh $simInput $outputName
-
-    read -p "Enter the kernel to run on the synthetic files: " kernel
-    echo ""
-
-    echo "Running SKAT"
-    Rscript RunSkat.R $outputName.bed $outputName.bim $outputName.fam $outputName.bim.SetID "SSD_File.SSD" $kernel $method
-    echo "SKAT complete."
-    mv $outputName.* ../output/$outputName.*
-
-
-  else
-    echo "Unknown input."
-  fi
 done
