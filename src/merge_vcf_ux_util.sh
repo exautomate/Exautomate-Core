46fb38a41213934d2b83155b743559190f31d603
jdron <jdron@users.noreply.github.com>
2018-04-20 11:51:52 -0400

    Updates

--- src/merge_vcf_ux_util.sh
+++ src/merge_vcf_ux_util.sh
@@ -26,7 +26,7 @@ echo "---- Divisor ----"
 
 
 choice="empty"
-echo "Enter the filenames to be removed, one at a time followed by enter/return, or hit enter to continue."
+echo "Enter the filenames to be removed, one at a time followed by enter/return, or type 'done' to continue"
 
 cd ../input/merge/
 mkdir movedfiles
@@ -38,12 +38,9 @@ do
 
 done
 
-
-done
-
   echo "--- Running Tabix ---"
   echo ""
-  for i in *.vcf.*
+  for i in *.vcf.gz
   do
     tabix $i
   done
@@ -53,9 +50,42 @@ done
   echo "--- Running vcf-concat ---"
   echo ""
   #Using --pad-missing [ . in place of missing columns ] and -s to allow small overlaps in files.
-  vcf-concat -p -s *.vcf.* | gzip -c > merged.vcf.gz
+  #vcf-concat -p -s *.vcf.gz | bgzip -c > merged.vcf.gz
+  #vcf-merge -R "0|0" *.vcf.gz | bgzip -c > merged.vcf.gz
+
+ #java -Xmx16g -jar ../../dependencies/GenomeAnalysisTK.jar -T CombineVariants -R ../../dependencies/hg19.fasta -V 1116E.clc.vcf.gz -V 1188E.clc.vcf.gz -o iteration1.vcf.gz -genotypeMergeOptions UNIQUIFY -env
+
+count=1
+for i in *.vcf.gz
+do
+
+  if [ $count -eq 1 ]
+  then
+    tmpfile=$i
+  elif [$count -eq 2 ]
+  then
+    java -Xmx16g -jar ../../dependencies/GenomeAnalysisTK.jar -T CombineVariants -R ../../dependencies/hg19.fasta -V $tmpfile -V $i -o $count.vcf.gz -genotypeMergeOptions UNIQUIFY -env
+  else
+  then
+    subcount=$((count-1));
+    java -Xmx16g -jar ../../dependencies/GenomeAnalysisTK.jar -T CombineVariants -R ../../dependencies/hg19.fasta -V $subcount.vcf.gz -V $i -o $count.vcf.gz -genotypeMergeOptions UNIQUIFY -env
+
+  fi
+
+  count=$((count+1));
+
+done
+
+
+
   echo ""
   echo "--- Finishing vcf-concat ---"
 
   #Put files back.
   mv /movedfiles/* ./
+
+
+#  for i in *.vcf
+# do
+# sed -i 's/E_R1.*$/E_R1/g' $i
+# done
