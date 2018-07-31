7aac10374b5d5b301441a6bc9eab2c14b0a57bd1
jdron <jdron@users.noreply.github.com>
2018-07-31 08:48:43 -0400

    out.log

--- src/output_test.sh
+++ src/output_test.sh
@@ -2,11 +2,30 @@
 
 ### testing output log
 
-## for real directory: ../output/out.log
-LOGFILE=./out.log
+LOGFILE=../output/out.log
+
+echo "#################### OUTPUT LOG #################### \n \n" >> $LOGFILE
 
 
-echo "$(date "+%m%d%Y %T"): Start" >> $LOGFILE
+echo "$(date "+%m%d%Y %T"): Starting Exautomate \n" >> $LOGFILE
+
+echo "####### OPTION 1: Pre-merged .vcf for analysis ####### \n" >> $LOGFILE
+echo "Input .vcf: ________ \n" >> $LOGFILE
+echo "Number of controls: _____ \n" >> $LOGFILE
+echo "Output .vcf: ________ \n" >> $LOGFILE
+echo "Output PLINK files: ________ \n" >> $LOGFILE
+echo "Kernal option: ________ \n" >> $LOGFILE
+echo "Test: ________ \n" >> $LOGFILE
+
+#other output? like from annovar or plink?
 
 
-echo "$(date "+%m%d%Y %T"): End" >> $LOGFILE
+echo "####### OPTION 1: Pre-merged .vcf for analysis ####### \n" >> $LOGFILE
+
+echo "####### OPTION 2: Merge case and control .vcf for analysis ####### \n" >> $LOGFILE
+
+echo "####### OPTION 3: Retrieve 1000 Genome ####### \n" >> $LOGFILE
+
+echo "####### OPTION 4: Synthetic run ####### \n" >> $LOGFILE
+
+echo "$(date "+%m%d%Y %T"): Ending Exautomate" >> $LOGFILE
